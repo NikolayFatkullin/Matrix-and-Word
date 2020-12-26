9c3d19ddfcefecce31396313d95578cff2173563
nikelok01@gmail.com <163amefeZ>
2020-12-26 19:04:05 +0200

    - Created class MatrixAndWord; - Created class MatrixAndWordTest; - In class MatrixAndWord, created methods: getSequence(), stringToMatrix(), and also created constants; - Created tests for testing program;

--- src/main/java/MatrixAndWord.java
+++ src/main/java/MatrixAndWord.java
@@ -1,5 +1,77 @@
-import java.util.*;
-
 public class MatrixAndWord {
+    private static final int VARIABLE_TO_CHECK_LENGTH = 1;
+    private static final int[] ROWS_ARRAY_STEPS = new int[]{-1, 0, 1, 0};
+    private static final int[] COLUMN_ARRAY_STEPS = new int[]{0, 1, 0, -1};
+    private static final String STRING_FORMAT_FOR_RESULT = "[%d,%d]";
 
+    public String getSequence(String firstWord, String secondWord) {
+        if (firstWord == null || secondWord == null || secondWord.equals("")) {
+            return "";
+        }
+        if (firstWord.length() <= 1 && firstWord.equals(secondWord)) {
+            return "[0,0]";
+        }
+        if (Math.sqrt(firstWord.length()) % VARIABLE_TO_CHECK_LENGTH != 0) {
+            throw new RuntimeException("Incorrect size of first word!");
+        }
+        if (secondWord.length() > firstWord.length()) {
+            throw new RuntimeException("Incorrect size of second word!");
+        }
+        char[][] wordsMatrix = stringToMatrix(firstWord);
+        String resultedSequence;
+        for (int i = 0; i < wordsMatrix.length; i++) {
+            for (int j = 0; j < wordsMatrix.length; j++) {
+                resultedSequence = searchInMatrix(wordsMatrix, i, j, secondWord, 0);
+                if (!resultedSequence.equals("Incorrect")) {
+                    return resultedSequence;
+                }
+            }
+        }
+        return "No matches founded!";
+    }
+
+    private char[][] stringToMatrix(String word) {
+        int matrixLength = (int) Math.sqrt(word.length());
+        char[] temporaryArray = word.toUpperCase().toCharArray();
+        char[][] matrix = new char[matrixLength][matrixLength];
+        int indexForArray = 0;
+        for (int i = 0; i < matrixLength; i++) {
+            for (int j = 0; j < matrixLength; j++) {
+                matrix[i][j] = temporaryArray[indexForArray];
+                indexForArray++;
+            }
+        }
+        return matrix;
+    }
+
+    private String searchInMatrix(char[][] wordsMatrix, int rows, int columns,
+                                  String word, int charIndex) {
+        if (wordsMatrix[rows][columns] != word.charAt(charIndex)) {
+            return "Incorrect";
+        }
+        StringBuilder result = new StringBuilder();
+        result.append(String.format(STRING_FORMAT_FOR_RESULT, rows, columns));
+        if (charIndex >= word.length() - 1) {
+            return result.toString();
+        }
+        char tmp = wordsMatrix[rows][columns];
+        wordsMatrix[rows][columns] = '#';
+
+        for (int i = 0; i < 4; i++) {
+            int checkedRow = rows + ROWS_ARRAY_STEPS[i];
+            int checkedColumn = columns + COLUMN_ARRAY_STEPS[i];
+            if (checkedRow >= 0 && checkedRow < wordsMatrix.length
+                    && checkedColumn >= 0 && checkedColumn < wordsMatrix[0].length
+                    && wordsMatrix[checkedRow][checkedColumn] == word.charAt(charIndex + 1)) {
+                String temporaryResult = searchInMatrix(wordsMatrix, checkedRow, checkedColumn,
+                        word, charIndex + 1);
+                if (!temporaryResult.equals("Incorrect")) {
+                    result.append("->").append(temporaryResult);
+                    return result.toString();
+                }
+            }
+        }
+        wordsMatrix[rows][columns] = tmp;
+        return "Incorrect";
+    }
 }
---
+++ src/test/java/MatrixAndWordTest.java
@@ -0,0 +1,78 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class MatrixAndWordTest {
+    private static MatrixAndWord matrix;
+
+    @BeforeAll
+    public static void beforeAll() {
+        matrix = new MatrixAndWord();
+    }
+
+    @Test
+    public void testForInputNullAndEmpty() {
+        String expected = "";
+        String actual = matrix.getSequence(null, "KING");
+        String secondActual = matrix.getSequence("QWERTSFFT", null);
+        String thirdActual = matrix.getSequence("", "");
+        assertEquals(expected, actual,
+                String.format("You should return empty String, but you returned %s", actual));
+        assertEquals(expected, secondActual,
+                String.format("You should return empty String, but you returned %s", secondActual));
+        assertEquals(expected, thirdActual,
+                String.format("You should return empty String, but you returned %s", thirdActual));
+        String fourthActual = matrix.getSequence("KLKLK", "");
+        assertEquals(expected, fourthActual,
+                String.format("You should return empty String, but you returned %s", fourthActual));
+    }
+
+    @Test
+    public void testForOneLetterInInputWord() {
+        String firstExpected = "[0,0]";
+        String firstActual = matrix.getSequence("K", "K");
+        assertEquals(firstExpected, firstActual,
+                String.format("You should return %s, but you returned %s",
+                        firstExpected, firstActual));
+    }
+
+    @Test
+    public void incorrectLengthOfWords() {
+        assertThrows(RuntimeException.class, () -> matrix.getSequence("KAASASASAS", "LA"));
+        assertThrows(RuntimeException.class, () -> matrix.getSequence("K", "LAAAAAAAAA"));
+        assertThrows(RuntimeException.class, () -> matrix.getSequence("", "LA"));
+    }
+
+    @Test
+    void correctValues_OK() {
+        String firstExpected = "[1,2]->[1,3]->[0,3]->[0,2]";
+        String secondExpected = "[3,2]->[3,3]->[2,3]->[1,3]->[1,4]->[1,5]->[2,5]->[2,4]";
+        String thirdExpected = "[7,5]->[6,5]->[5,5]->[5,6]->[6,6]->[7,6]->[8,6]->[9,6]->[10,6]"
+                + "->[11,6]->[11,7]->[11,8]" + "->[11,9]->[11,10]->[11,11]";
+        String firstActual = matrix.getSequence("QLGNAEKIRLRNGEAE", "KING");
+        String secondActual = matrix.getSequence("ADGGHEETUPUTCDNMREMOCODHPRAAGNCDCCCN",
+                "COMPUTER");
+        String thirdActual = matrix.getSequence("EQWEAfSDFFXZGFQWQoLCKAZXCCASWe)Vf@COFNVDDu(4N#YGD"
+                        + "BCERg17gSAVPDSWTAG%YLPQaqVAAEUGxTKFdwXSMLEXM[JWneVASeOBNGJDerXZXaFCDFH"
+                        + "XgtXZNgLMD]GAelfoeueGends",
+                "LEAGUEOFLEGENDS");
+        assertEquals(firstExpected, firstActual,
+                String.format("You should return %s, but you returned %s",
+                        firstExpected, firstActual));
+        assertEquals(secondExpected, secondActual,
+                String.format("You should return %s, but you returned %s",
+                        secondExpected, secondActual));
+        assertEquals(thirdExpected, thirdActual, String.format("You should return %s, "
+                        + "but you returned %s",
+                thirdExpected, thirdActual));
+        String fourthExpected = "No matches founded!";
+        String fourthActual = matrix.getSequence("EQWEAfSDFFXZGFQWQoLCKAZXCCASWe)Vf@COFNVDDu(4N#"
+                        + "YGDBCERg17gSAVPDSWTAG%YLPQaqVAAEUGxTKFdwXSMLEXM[JWneVASeOBNGJDerXZXa"
+                + "FCDFHXgtXZNgLMD]GAelfoeueGeER)", "PHENOMENA");
+        assertEquals(fourthExpected, fourthActual, String.format("You should return %s, "
+                        + "but you returned %s",
+                fourthExpected, fourthActual));
+    }
+}
